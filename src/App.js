b9e29c3de91601d10bb808d0834a76a03a5605df
Dominika Porzyczka <dporz@softserveinc.com>
2018-09-10 16:06:44 +0200

    Add Playlist component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Playlist from './components/Playlist/Playlist';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Playlist />
 
       </div>
     );
