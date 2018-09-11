c37c2a0b40f0b6a8241be12605bf0417a2dfba0f
mgiel-softserve <42805527+mgiel-softserve@users.noreply.github.com>
2018-09-11 18:49:12 +0300

    Feature/playlist mobx (#6)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Playlist from './components/Playlist/Playlist';
+import Searcher from './components/Searcher/Searcher';
+import Player from './components/Player/Player';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Searcher />
+        <Playlist />
+        <Player />
       </div>
     );
   }
