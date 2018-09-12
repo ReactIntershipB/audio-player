387a1e6fe8329a1c5f7f9f5f9eae35549c0e6546
wmete-softserve <wmete@softserveinc.com>
2018-09-12 16:51:04 +0200

    Add API functions to Models

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,5 +1,20 @@
 import { observable } from 'mobx';
 
+import { CORS_ALLOW_URL, BASE_URL } from "../config/api_config";
+
 export class Model {
     @observable isInitialized = false;
+
+    @action
+    getAPIBaseURL = (CORS_ALLOW_URL, BASE_URL) => {
+        return `${CORS_ALLOW_URL}${BASE_URL}`;
+    }
+
+    @action
+    getSongsFromAPI = (getAPIBaseURL, term) => {
+        return axios.get(`${getAPIBaseURL}${term}`)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
+    }
+
 }
--- src/models/SearcherModel/SearcherModel.js
+++ src/models/SearcherModel/SearcherModel.js
@@ -1,5 +1,16 @@
 import Model from './../Model';
+import { observable, action } from 'mobx';
+
+import { getSongsFromAPI } from '../Model';
 
 export class SearcherModel extends Model {
-  
+    @observable term = "";
+
+    @action
+    findSongs(term) {
+        
+        getSongsFromAPI(term)
+            .then(songsRes => console.log(songsRes))
+            .catch(err => console.log(err));
+    }
 }
