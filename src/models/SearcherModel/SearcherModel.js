b87ca237518556d6805de2cee04a194d19c397c6
wmete-softserve <42337078+wmete-softserve@users.noreply.github.com>
2018-09-13 09:16:44 +0200

    Access to Deezer API (#12)

--- src/models/SearcherModel/SearcherModel.js
+++ src/models/SearcherModel/SearcherModel.js
@@ -1,5 +1,19 @@
-import Model from './../Model';
+import { Model } from './../Model';
+import { observable, action } from 'mobx';
 
 export class SearcherModel extends Model {
-  
+    constructor() {
+        super();
+
+        this.model = new Model();
+    }
+
+    @observable term = '';
+
+    @action
+    findSongs(term) {
+        this.model.getSongsFromAPI(this.model.getAPIBaseURL, term)
+            .then(songsRes => console.log(songsRes))
+            .catch(err => console.log(err));
+    }
 }
