b5695c8bd8e6589fd3b5d6c7d53d4196abb2fefa
wtart-softserve <wtart@softserveinc.com>
2018-09-13 12:07:51 +0300

    Moving models to models folder

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,38 +3,8 @@ import PropTypes from 'prop-types';
 import { Avatar, Button, Col, Row, Slider } from 'antd';
 import { observer } from 'mobx-react';
 import { observable, action, autorun } from 'mobx';
+import { PlayerModel } from '../../models/PlayerModel/PlayerModel';
 import './Player.css';
-
-const data = [
-  {
-      id: 0,
-      title: 'No trailing space',
-      author: 'Linter',
-      album: 'Chocolate cake dessert sweet roll jujubes',
-      time: 194
-  },
-  {
-      id: 1,
-      title: 'I\'m forgotten',
-      author: 'Var',
-      album: 'Lollipop chupa chups tart bonbon',
-      time: 160
-  },
-  {
-      id: 2,
-      title: 'I don\'t have this',
-      author: 'Arrow function',
-      album: 'Gummi bears wafer pastry macaroon icing biscuit',
-      time: 242
-  },
-  {
-      id: 3,
-      title: 'Deadline is coming',
-      author: 'Scrum master',
-      album: 'Jujubes caramels jelly carrot cake',
-      time: 198
-  }
-];
 
 @observer
 class Player extends React.Component {
@@ -204,27 +174,3 @@ class PlayerUI {
 }
 
 export default Player;
-
-class PlayerModel {
-  @observable
-  track;
-
-  init = (songId) => {
-    return this.find(songId);
-  }
-
-  find = (songId) => {
-    return this.fetch(songId).then(song => {
-      this.track = song;
-    });
-  }
-
-  fetch = (songId) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const matchedSong = data.filter(song => song.id === songId);
-        if (matchedSong.length) resolve(matchedSong[0]);
-      }, 500);
-    });
-  }
-}
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -4,38 +4,8 @@ import { List, Button, Avatar } from 'antd';
 import { observer } from 'mobx-react';
 import { observable, action, reaction } from 'mobx';
 
+import { PlaylistModel } from '../../models/PlaylistModel/PlaylistModel';
 import './Playlist.css';
-
-const data = [
-    {
-        id: 0,
-        title: 'No trailing space',
-        author: 'Linter',
-        album: 'Chocolate cake dessert sweet roll jujubes',
-        time: 194
-    },
-    {
-        id: 1,
-        title: 'I\'m forgotten',
-        author: 'Var',
-        album: 'Lollipop chupa chups tart bonbon',
-        time: 160
-    },
-    {
-        id: 2,
-        title: 'I don\'t have this',
-        author: 'Arrow function',
-        album: 'Gummi bears wafer pastry macaroon icing biscuit',
-        time: 242
-    },
-    {
-        id: 3,
-        title: 'Deadline is coming',
-        author: 'Scrum master',
-        album: 'Jujubes caramels jelly carrot cake',
-        time: 198
-    }
-  ];
 
 @observer
 class Playlist extends Component {
@@ -122,24 +92,6 @@ class PlaylistUI {
     @action
     updateCurrentSong = (id) => {
         this.currentlyPlaying = id;
-    }
-}
-
-class PlaylistModel {
-    @observable
-    playlist = [];
-
-    init = () => {
-      return this.fetchPlaylist();
-    }
-
-    fetchPlaylist = () => {
-      return new Promise((resolve, reject) => {
-          setTimeout(() => {
-              this.playlist = data;
-              resolve(true);
-          }, 2000);
-      });
     }
 }
 
