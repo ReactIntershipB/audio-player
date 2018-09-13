438aa909acd20458b1fcd7a077a1ec5495ac4d73
dominikaporzyczkaSS <dporz@softserveinc.com>
2018-09-13 09:54:55 +0200

    Resolve conflict in PlaylistModel

--- src/models/PlaylistModel/PlaylistModel.js
+++ src/models/PlaylistModel/PlaylistModel.js
@@ -2,8 +2,4 @@ import { Model } from './../Model';
 
 export class PlaylistModel extends Model {
 
-<<<<<<< cd099f67ee5f38a3632808c239b22d9223dfe6cc
 }
-=======
-}
->>>>>>> Move SearcherModel to separate file
