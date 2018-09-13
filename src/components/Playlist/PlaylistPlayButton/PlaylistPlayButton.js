1375d7bdd01b5f5fd18e39d8760756fccdff5c66
dominikaporzyczkaSS <dporz@softserveinc.com>
2018-09-13 17:18:46 +0200

    Fix white spaces

--- src/components/Playlist/PlaylistPlayButton/PlaylistPlayButton.js
+++ src/components/Playlist/PlaylistPlayButton/PlaylistPlayButton.js
@@ -3,19 +3,19 @@ import { PropTypes } from 'prop-types';
 import { Button } from 'antd';
 
 export const PlaylistPlayButton = (props) => {
-    return (
-        <Button
-            type='primary'
-            shape='circle'
-            icon={props.icon}
-            size='large'
-            onClick={props.onClick}
-        />
-    );
+  return (
+    <Button
+      type='primary'
+      shape='circle'
+      icon={props.icon}
+      size='large'
+      onClick={props.onClick}
+    />
+  );
 };
 
 PlaylistPlayButton.propTypes = {
-    item: PropTypes.object,
-    icon: PropTypes.string,
-    onClick: PropTypes.function
+  item: PropTypes.object,
+  icon: PropTypes.string,
+  onClick: PropTypes.function
 };
