688c29233140a1401e78d7f37be4f9de4c957088
wmete-softserve <42337078+wmete-softserve@users.noreply.github.com>
2018-09-17 13:14:14 +0200

    Api query change (#33)

--- src/models/SearchModel/SearchModel.js
+++ src/models/SearchModel/SearchModel.js
@@ -5,9 +5,11 @@ export class SearchModel extends Model {
     @observable term = '';
     @observable filterName = 'artist';
 
+    staticQueryURL = 'search?q=';
+
     @action
     find(term, filterName) {
-      this.getData(term, filterName);
+      this.getData(`${this.staticQueryURL}${filterName}:"${term}"`);
     }
 
     @action
