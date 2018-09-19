d17af1c61eea846253eba85b49bb598abbcab02b
wtart-softserve <wtart@softserveinc.com>
2018-09-19 14:01:45 +0300

    style changes to list column, WIP

--- src/components/common/ListColumn.js
+++ src/components/common/ListColumn.js
@@ -1,46 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List, Avatar } from 'antd';
-
 import { PlayIcon } from './PlayIcon';
 import './Common.css';
 
 const ListComponent = ({ heading, data, avatar }) => {
   const ui = new ListUi();
-  if (data.length === 0) {
-      return (
-        <div className="no-results-container">
-            <i className="far fa-sad-tear"></i>
-            <p>No Results</p>
-        </div>
-      );
-  } else {
     return (
-        <div className='playlist-container'>
-            <div className='avatar'>
-                {avatar && <Avatar shape='square' size={64} src={avatar} />}
-                <h2>{heading}</h2>
-            </div>
-            <div>
-                {data.map(item => {
-                    const title = item.album ? item.album.title : item.title;
-                    return (
-                        <div key={item.id}>
-                        <List.Item>
-                                <List.Item.Meta
-                                    avatar={<PlayIcon songId={item.id}/>}
-                                    title={item.title}
-                                    description={ui.getDescription(item.artist.name, title)}/>
-                                <div>{ui.getDuration(item.duration)}</div>
-                        </List.Item>
-                        <hr />
-                        </div>
-                    );
-                })}
-            </div>
+      <div className="playlist-container">
+        <div className="list-column-container">
+          <h2 className="list-column-title">{heading}</h2>
+          {data.map(item => {
+            const { title } = item.album ? item.album : item;
+            return (
+              <div key={item.id} className="list-column-item-container">
+                <div className="list-column-item-content">
+                  <PlayIcon songId="item.id" />
+                  <h3>{title}</h3>
+                  <p>{ui.getDescription(item.artist.name, title)}</p>
+                  <p>{ui.getDuration(item.duration)}</p>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
         </div>
-      );
-  }
+      </div>
+    );
 };
 
 ListComponent.propTypes = {
