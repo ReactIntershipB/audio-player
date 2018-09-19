21c296735f73ea5f55b25674a1d16b1f8c6c6e07
wmete-softserve <wmete@softserveinc.com>
2018-09-19 12:32:50 +0200

    Add style for Search panel

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Icon, Menu, Input, Radio } from 'antd';
+import { Menu, Input, Radio } from 'antd';
 import { observer, inject } from 'mobx-react';
 import PropTypes from 'prop-types';
-import './Search.css';
 
-const RadioGroup = Radio.Group;
+import './Search.css';
 
 @inject('searchModel', 'appUI')
 @observer
@@ -15,12 +14,12 @@ class SearchComponent extends Component {
 
     get dropdownMenu() {
         return (
-            <Menu
-                className='dropdown-menu'
-                value={this.props.searchModel.filterName}
-                onClick={this.onFilterClick}
-                selectedKeys={[this.props.searchModel.filterName]}
-            >
+          <Menu
+            className='dropdown-menu'
+            value={this.props.searchModel.filterName}
+            onClick={this.onFilterClick}
+            selectedKeys={[this.props.searchModel.filterName]}
+          >
             <Menu.Item key="artist" value='artist'>
                 Artist
             </Menu.Item>
@@ -30,30 +29,20 @@ class SearchComponent extends Component {
             <Menu.Item key="track" value='track'>
                 Track
             </Menu.Item>
-        </Menu>
-        );
-    };
-
-    get addonSearchIcon() {
-        return (
-            <Icon type="search" />
-        );
+          </Menu>
+      );
     };
 
     fetchData = () => {
       const { term, type } = this.props.match.params;
+
       if (term && type) {
         this.props.searchModel.find(term, type);
       }
     }
 
-    getFilterName = (filterName) => {
-      return filterName.charAt(0).toUpperCase() + filterName.slice(1);
-    }
-
     onInputChange = e => {
       this.props.searchModel.inputChange(e.target.value);
-      this.props.appUI.changeButtonStatus(e.target.value);
     }
 
     onFilterClick = value => {
@@ -67,7 +56,6 @@ class SearchComponent extends Component {
         this.props.searchModel.find(value, filterName);
         this.props.searchModel.setTermText(value);
         this.clearInput();
-        this.props.appUI.changeButtonStatus(value);
         this.props.history.push(`/search/${filterName}/${value}`);
       }
     }
@@ -77,36 +65,27 @@ class SearchComponent extends Component {
     }
 
     render() {
-      const { filterName } = this.props.searchModel;
       return (
         <div className="search-container">
-            <div className='search-form-container'>
+          <div className='search-form-container'>
             <Input.Search
-              placeholder="input search text"
-              onSearch={value => this.onSubmitClick(value)}
+              placeholder={`Find your favourite ${this.props.searchModel.filterName}`}
+              className="search-input"
+              value={this.props.searchModel.term}
+              onChange={this.onInputChange}
+              onSearch={this.onSubmitClick}
               enterButton
-              />
-            <RadioGroup onChange={this.onFilterClick} value={filterName}>
-              <Radio value={'artist'}>Artist</Radio>
-              <Radio value={'album'}>Album</Radio>
-              <Radio value={'track'}>Track</Radio>
-            </RadioGroup>
-            {/* <Dropdown
-              className='dropdown'
-              overlay={this.dropdownMenu}
-            >
-              <Button className="ant-dropdown-link">
-                {this.getFilterName(this.props.searchModel.filterName)}
-                <Icon type="down" />
-              </Button>
-            </Dropdown> */}
-            {/* <Button
-              type="primary"
-              disabled={this.props.appUI.isButtonDisabled}
-              onClick={this.onSubmitClick}
-            >
-              Submit
-            </Button> */}
+            />
+            <Radio.Group
+                onChange={this.onFilterClick}
+                className="radio-group-input"
+                value={this.props.searchModel.filterName}
+                buttonStyle="solid"
+              >
+              <Radio.Button value='artist'>Artist</Radio.Button>
+              <Radio.Button value='album'>Album</Radio.Button>
+              <Radio.Button value='track'>Track</Radio.Button>
+            </Radio.Group>
           </div>
         </div>
       );
