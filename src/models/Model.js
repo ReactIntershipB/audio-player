72706314199e637fd53974889ca484e434bee424
wtart-softserve <wtart@softserveinc.com>
2018-09-20 17:42:47 +0300

    removing console

--- src/models/Model.js
+++ src/models/Model.js
@@ -13,7 +13,6 @@ export class Model {
     @action
     getData = (apiQuery) => {
         this.toggleLoading();
-        console.trace();
         this.isError = false;
         return axios.get(`${this.getAPIBaseURL}${apiQuery}`)
             .then(res => {
